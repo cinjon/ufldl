343e62da6b68de1388541e42693c04964468d148
cinjon <cinjon.resnick@gmail.com>
2014-06-10 13:39:12 -0400

    stacked autoencoders: two old helper files + work on stackedAECost

---
+++ stackedae_exercise/computeNumericalGradient.m
@@ -0,0 +1,37 @@
+function numgrad = computeNumericalGradient(J, theta)
+% numgrad = computeNumericalGradient(J, theta)
+% theta: a vector of parameters
+% J: a function that outputs a real-number. Calling y = J(theta) will return the
+% function value at theta. 
+  
+% Initialize numgrad with zeros
+numgrad = zeros(size(theta));
+zeroVector = zeros(size(theta));
+
+%% ---------- YOUR CODE HERE --------------------------------------
+% Instructions: 
+% Implement numerical gradient checking, and return the result in numgrad.  
+% (See Section 2.3 of the lecture notes.)
+% You should write code so that numgrad(i) is (the numerical approximation to) the 
+% partial derivative of J with respect to the i-th input argument, evaluated at theta.  
+% I.e., numgrad(i) should be the (approximately) the partial derivative of J with 
+% respect to theta(i).
+%                
+% Hint: You will probably want to compute the elements of numgrad one at a time. 
+
+epsilon = 10^-4;
+for i=1:size(theta),
+  basisVector = zeroVector;
+  basisVector(i) = 1;
+  positiveVector = theta + epsilon*basisVector;
+  negativeVector = theta - epsilon*basisVector;
+  numgrad(i) = (J(positiveVector) - J(negativeVector))/(2*epsilon);
+end
+
+
+
+
+
+
+%% ---------------------------------------------------------------
+end
--- stackedae_exercise/stackedAECost.m
+++ stackedae_exercise/stackedAECost.m
@@ -1,18 +1,18 @@
 function [ cost, grad ] = stackedAECost(theta, inputSize, hiddenSize, ...
                                               numClasses, netconfig, ...
                                               lambda, data, labels)
-                                         
+
 % stackedAECost: Takes a trained softmaxTheta and a training data set with labels,
 % and returns cost and gradient using a stacked autoencoder model. Used for
 % finetuning.
-                                         
+
 % theta: trained weights from the autoencoder
 % visibleSize: the number of input units
 % hiddenSize:  the number of hidden units *at the 2nd layer*
 % numClasses:  the number of categories
 % netconfig:   the network configuration of the stack
 % lambda:      the weight regularization penalty
-% data: Our matrix containing the training data as columns.  So, data(:,i) is the i-th training example. 
+% data: Our matrix containing the training data as columns.  So, data(:,i) is the i-th training example.
 % labels: A vector containing labels, where labels(i) is the label for the
 % i-th training example
 
@@ -41,7 +41,7 @@ groundTruth = full(sparse(labels, 1:M, 1));
 
 
 %% --------------------------- YOUR CODE HERE -----------------------------
-%  Instructions: Compute the cost function and gradient vector for 
+%  Instructions: Compute the cost function and gradient vector for
 %                the stacked autoencoder.
 %
 %                You are given a stack variable which is a cell-array of
@@ -60,6 +60,45 @@ groundTruth = full(sparse(labels, 1:M, 1));
 %                Note that the size of the matrices in stackgrad should
 %                match exactly that of the size of the matrices in stack.
 %
+
+% stack{d}.s = delta, stack{d}.z = result, stack{d}.a = activation of result
+
+% Compute the forward activations
+stack{1}.a = data;
+for d = 1:numel(stack)
+    stack{d+1}.z = stack{d}.w * stack{d}.a + repmat(stack{d}.b, 1, \
+                                                  size(stack{d}.a, 2));
+    stack{d+1}.a = sigmoid(stack{d+1}.z);
+end
+
+% Compute the vector of conditional probabilities, direct from softmaxCost calculation
+P = softmaxTheta * stack{numel(stack) + 1}.a;
+P = bsxfun(@minus, P, max(P));
+P = exp(P);
+P = bsxfun(@rdivide, P, sum(P));
+
+% Compute the errors due to each node
+stack{numel(stack)+1}.s = -softmaxTheta' * (groundTruth - \
+                                        P) * \
+                         sigmoidDeriv(stack{numel(stack)}.a);
+for d = numel(stack):-1:3
+    stack{d}.s = stack{d}.w * stack{d+1}.s * sigmoidDeriv(stack{d}.a);
+end
+
+% Compute the partial derivatives
+for d = 1:numel(stack)
+    stackgrad{d}.w = stack{d+1}.s * stack{d}.a';
+    stackgrad{d}.b = stack{d+1}.s;
+end
+
+indicator_cost = groundTruth .* log(P);
+indicator_cost = -1/numCases * sum(indicator_cost(:));
+cost = indicator_cost + lambda/2 * sum((softmaxTheta.^2)(:)); % Weight decay
+
+softmaxThetaGrad = -1/numCases * ((groundTruth - P) * stack{numel(stack)+1}.a') + (lambda * theta);
+
+
+
 
 
 
@@ -86,4 +125,8 @@ end
 % You might find this useful
 function sigm = sigmoid(x)
     sigm = 1 ./ (1 + exp(-x));
+end
+
+function deriv = sigmoidDeriv(x)
+  deriv = x .* (1-x);
 end
